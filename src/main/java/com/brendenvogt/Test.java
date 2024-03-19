359e3dc1960a6039b6df6c094b5a5a1bb8d414bc
Brenden Vogt <bvogtdev@gmail.com>
2024-03-18 23:33:40 -0700

    Added files

---
+++ src/main/java/com/brendenvogt/Test.java
@@ -0,0 +1,10 @@
+package com.brendenvogt;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("testing start");
+        Main.main(new String[]{"","",""});
+        System.out.println("testing done");
+    }
+}
